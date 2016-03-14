b8e8b258aa7825ca2700cc0dabc4d268c80437a5
Darko Fabijan <darko@renderedtext.com>
2016-03-14 18:10:54 +0100

    Working on specs

--- lib/tackle/worker.rb
+++ lib/tackle/worker.rb
@@ -5,34 +5,43 @@ module Tackle
   class Worker
     include Tackle::TackleLogger
 
+    attr_reader :rabbit
+
     def initialize(options = {})
       @options = options
       @logger = options[:logger]
+      @rabbit = Tackle::Rabbit.new(@options)
+    end
+
+    def connect
+      @rabbit.connect
     end
 
     def perform(&block)
-      rabbit = Tackle::Rabbit.new(@options)
-      rabbit.connect
-
       tackle_log("Subscribing to queue...")
-      rabbit.queue.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
+      rabbit.queue.subscribe(:manual_ack => true,
+                             :block => true) do |delivery_info, properties, payload|
 
-        tackle_log("Received message")
+        tackle_log("Received message. Processing...")
+        process_message(delivery_info, properties, payload, block)
+        tackle_log("Done with processing message.")
 
-        begin
-          tackle_log("Calling message processor...")
-          block.call(body)
-          rabbit.channel.ack(delivery_info.delivery_tag)
-          tackle_log("Successfully processed message")
-        rescue Exception => ex
-          tackle_log("Failed to process message. Received exception '#{ex}'")
-          try_again = Tackle::Retry.new(rabbit, delivery_info, properties, body, @options)
-          try_again.retry
-        end
       end
-
     rescue Interrupt => _
       rabbit.close
+    end
+
+    def process_message(delivery_info, properties, payload, block)
+      begin
+        tackle_log("Calling message processor...")
+        block.call(payload)
+        @rabbit.channel.ack(delivery_info.delivery_tag)
+        tackle_log("Successfully processed message")
+      rescue Exception => ex
+        tackle_log("Failed to process message. Received exception '#{ex}'")
+        try_again = Tackle::Retry.new(@rabbit, delivery_info, properties, payload, @options)
+        try_again.retry
+      end
     end
 
   end
--- spec/tackle/worker_spec.rb
+++ spec/tackle/worker_spec.rb
@@ -1,42 +1,72 @@
 require 'spec_helper'
 
 describe Tackle::Worker do
-  class DummyWorker
-    def initialize
-      logger = Logger.new(STDOUT)
-      @worker = Tackle::Worker.new(:url => "localhost",
-                                   :exchange => "test-exchange",
-                                   :queue => "test-queue",
-                                   :retry_limit => 10,
-                                   :logger => logger)
-    end
 
-    def run
-      @worker.perform do |message|
-        puts "got message: #{message}"
-        1 / 0
-        true
-      end
-    end
+  let(:conn) do
+    conn = Bunny.new
+    conn.start
+    conn
+  end
+
+  after :each do
+    conn.close if conn.open?
+  end
+
+  def send_message
+    channel = conn.create_channel
+    x = channel.fanout("test-exchange")
+    x.publish "x"
   end
 
   before do
-    @worker = DummyWorker.new
+    logger = Logger.new(STDOUT)
+    @worker = Tackle::Worker.new(:url => "localhost",
+                                 :exchange => "test-exchange",
+                                 :queue => "test-queue",
+                                 :retry_limit => 2,
+                                 :logger => logger)
+    @worker.connect
   end
 
-  it 'has a version number' do
-    @worker.run
+  describe "#process_message" do
+    context "without exceptions" do
+
+      it "processes message without retries" do
+        send_message
+        sleep(1)
+        delivery_info, properties, payload = @worker.rabbit.queue.pop
+        execution_queue = []
+        processor = lambda { |body| execution_queue << body * 2 }
+
+        @worker.process_message(delivery_info, properties, payload, processor)
+
+        expect(execution_queue).to eql(["xx"])
+      end
+
+    end
+
+    context "with exceptions" do
+
+      it "tries to process message two times" do
+        send_message
+        sleep(2)
+        delivery_info, properties, payload = @worker.rabbit.queue.pop
+        execution_queue = []
+        processor = Proc.new { |body| execution_queue << body * 2; raise ArgumentError }
+
+        @worker.process_message(delivery_info, properties, payload, processor)
+        expect(execution_queue).to eql(["xx"])
+
+        sleep(2)
+
+        @worker.connect
+        sleep(10)
+        delivery_info, properties, payload = @worker.rabbit.queue.pop
+        @worker.process_message(delivery_info, properties, payload, processor)
+        expect(execution_queue).to eql(["xx"])
+      end
+
+    end
   end
 
-  it "send message" do
-    require "bunny"
-
-    conn = Bunny.new
-    conn.start
-    channel = conn.create_channel
-
-    x = channel.fanout("test-exchange")
-
-    x.publish "nest"
-  end
 end
