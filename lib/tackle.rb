aeea5196672b123d31901401e1a9c325be113c2a
Igor Šarčević <igor@renderedtext.com>
2016-03-24 09:38:44 +0100

    Simple subscribe

--- lib/tackle.rb
+++ lib/tackle.rb
@@ -1,5 +1,28 @@
 require "tackle/version"
 
 module Tackle
+  require "tackle/worker"
 
+  def self.subscribe(options = {}, &block)
+    # required
+    exchange_name = options.fetch(:exchange)
+    routing_key   = options.fetch(:routing_key)
+    queue_name    = options.fetch(:queue)
+
+    # optional
+    amqp_url    = options[:url]
+    retry_limit = options[:retry_limit]
+    retry_delay = options[:retry_delay]
+    logger      = options[:logger]
+
+    worker = Tackle::Worker.new(exchange_name,
+                                routing_key,
+                                queue_name,
+                                :url => amqp_url,
+                                :retry_limit => retry_limit,
+                                :retry_delay => retry_delay,
+                                :logger => logger)
+
+    worker.subscribe(block)
+  end
 end
