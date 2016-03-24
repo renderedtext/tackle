eee5095888baa9c2fc4c8d361dbc0b7230f568d1
Igor Šarčević <igor@renderedtext.com>
2016-03-24 11:13:01 +0100

    Implement message publishing

--- lib/tackle/publisher.rb
+++ lib/tackle/publisher.rb
@@ -10,9 +10,40 @@ module Tackle
     end
 
     def publish(message)
-      tackle_log("Publishing message started to exchange='#{@exchange_name}' routing_key='#{@routing_key}'")
+      tackle_log("Publishing message started exchange='#{@exchange_name}' routing_key='#{@routing_key}'")
 
-      tackle_log("Publishing message finished to exchange='#{@exchange_name}' routing_key='#{@routing_key}'")
+      with_rabbit_connection do |conn|
+        channel = conn.create_channel
+        tackle_log("Created a communication channel")
+
+        exchange = channel.direct(@exchange_name)
+        tackle_log("Declared the exchange")
+
+        exchange.publish(message, :routing_key => @routing_key, :persistent => true)
+      end
+
+      tackle_log("Publishing message finished exchange='#{@exchange_name}' routing_key='#{@routing_key}'")
+    end
+
+    private
+
+    def with_rabbit_connection
+      tackle_log("Establishing rabbit connection")
+
+      conn = Bunny.new
+      conn.start
+
+      yield(conn)
+
+      tackle_log("Established rabbit connection")
+    rescue StandardError => ex
+      tackle_log("An exception occured while sending the message exception='#{ex.class.name}' message='#{ex.message}'")
+
+      raise ex
+    ensure
+      tackle_log("Clossing rabbit connection")
+
+      conn.close if conn
     end
 
   end
--- spec/lib/tackle/publisher_spec.rb
+++ spec/lib/tackle/publisher_spec.rb
@@ -7,31 +7,41 @@ describe Tackle::Publisher do
   describe "#publish" do
     before do
       @publisher = Tackle::Publisher.new(exchange_name, routing_key, "amqp://localhost:5672", Logger.new(STDOUT))
+
+      allow(@publisher).to receive(:tackle_log)
+
+      @worker = Tackle::Worker.new(exchange_name, routing_key, "publishing-test-queue")
     end
 
     it "logs the message publishing" do
-      expect(@publisher).to receive(:tackle_log).with("Publishing message started to exchange='test-exchange' routing_key='test-routing-key'")
+      expect(@publisher).to receive(:tackle_log).with(/Publishing message started/)
 
-      expect(@publisher).to receive(:tackle_log).with("Publishing message finished to exchange='test-exchange' routing_key='test-routing-key'")
+      expect(@publisher).to receive(:tackle_log).with(/Publishing message finished/)
 
       @publisher.publish("test-message")
     end
 
     it "pushes a message into the exchange" do
       @publisher.publish("test-message")
+
+      _, _, payload = @worker.rabbit.queue.pop(:manual_ack => true)
+
+      expect(payload).to eq("test-message")
     end
 
     context "when an exception occurs" do
       before do
-        # stub something with an exception
+        allow(Bunny).to receive(:new) { raise "test exception" }
       end
 
       it "logs the exception" do
+        expect(@publisher).to receive(:tackle_log).with("An exception occured while sending the message exception='RuntimeError' message='test exception'")
 
+        expect { @publisher.publish("test-message") }.to raise_exception("test exception")
       end
 
       it "re-raises the exception" do
-
+        expect { @publisher.publish("test-message") }.to raise_exception("test exception")
       end
     end
   end
